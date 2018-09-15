5ed480766f000611fefd965b727c7eedb7034eab
oliveyuan <43288472+oliveyuan@users.noreply.github.com>
2018-09-15 16:38:38 +0800

    Update practice_1.R

--- week_1/course_1/practice_1.R
+++ week_1/course_1/practice_1.R
@@ -6,16 +6,16 @@ bmi <- (my.weight.kg)/(my.height.m)^2
 if(bmi>=35){
   print(paste("Your bmi: ", bmi))
   print("重度肥胖!")
-}else if(bmi>=30&bmi<35){
+}else if(bmi>=30 && bmi<35){
   print(paste("Your bmi: ", bmi))
   print("中度肥胖!")
-}else if(bmi>=27&bmi<30){
+}else if(bmi>=27 &&　bmi<30){
   print(paste("Your bmi: ", bmi))
   print("輕度肥胖!")
-}else if(bmi>=24&bmi<27){
+}else if(bmi>=24 && bmi<27){
   print(paste("Your bmi: ", bmi))
   print("過重!")
-}else if(bmi>=18.5&bmi<24){
+}else if(bmi>=18.5 && bmi<24){
   print(paste("Your bmi: ", bmi))
   print("正常範圍!")
 }else if(bmi<18.5){
